645805ad0f052c9bf4c4070ea675071fe17100f1
hardyJ01 <connect.hardip.com>
2026-04-01 21:12:52 +0530

    Basic utils function config

---
+++ backend/src/utils/ApiError.js
@@ -0,0 +1,18 @@
+class ApiError extends Error {
+    constructor(statusCode, message = "Something wenet wrong", errors=[],stack=""){
+        super(messsage);
+        this.statusCode = statusCode;
+        this.data= null;
+        this.message = message;
+        this.success = false;
+        this.errors = errors;
+        if(stack){
+            this.stack = stack;
+        }
+        else{
+            Error.captureStackTrace(this,this.constructor);
+        }
+
+    }
+}
+export {ApiError};
